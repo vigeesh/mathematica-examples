4e585b6f13e0ae2f647ffe113b1e86f94f9a9078
Vigeesh <vigeesh@kis.uni-freiburg.de>
2015-04-05 18:53:14 +0200

    First commit

---
+++ hankel_legendre.m
@@ -0,0 +1,49 @@
+(* ::Package:: *)
+
+(* ::Section:: *)
+(*Hankel function mapped on to the surface of a sphere*)
+
+
+(* ::Text:: *)
+(*Spherical Harmonics and the two components of the Hankel functions*)
+
+
+(* ::Input:: *)
+(**)
+(*l=20;*)
+(*m=5;*)
+(*f=Re;*)
+(*t=1;*)
+(*gradients=ColorData["RedGreenSplit"];*)
+(*SphericalPlot3D[1,{\[Theta],0,\[Pi]},{\[Phi],0,2 \[Pi]},*)
+(*ColorFunction->(gradients[t (.5+f[SphericalHarmonicY[l,m,#4,#5]])]&),Mesh->{17},MeshStyle->Opacity[0.2],Boxed->False,Axes->False,ColorFunctionScaling->False,PlotPoints->200,SphericalRegion->True,ViewAngle->.3,ImageSize->100]*)
+(*SphericalPlot3D[1,{\[Theta],0,\[Pi]},{\[Phi],0,2 \[Pi]},*)
+(*ColorFunction->(gradients[t (.5+f[(HankelH1[m,Sqrt[l(l+1)]*#4] )*Exp[I*m*#5]])]&),Mesh->{17},MeshStyle->Opacity[0.2],*)
+(*Boxed->False,Axes->False,ColorFunctionScaling->False,PlotPoints->200,SphericalRegion->True,ViewAngle->.3,ImageSize->100]*)
+(*SphericalPlot3D[1,{\[Theta],0,\[Pi]},{\[Phi],0,2 \[Pi]},*)
+(*ColorFunction->(gradients[t (.5+f[(HankelH2[m,Sqrt[l(l+1)]*#4])*Exp[I*m*#5]])]&),Mesh->{17},MeshStyle->Opacity[0.2],*)
+(*Boxed->False,Axes->False,ColorFunctionScaling->False,PlotPoints->200,SphericalRegion->True,ViewAngle->.3,ImageSize->100]*)
+
+
+(* ::Section:: *)
+(*Legendre function*)
+
+
+(* ::Text:: *)
+(*Spherical Harmonics and the two components of the Legendre functions*)
+
+
+(* ::Input:: *)
+(*l=20;*)
+(*m=5;*)
+(*f=Re;*)
+(*t=1;*)
+(*gradients=ColorData["RedGreenSplit"];*)
+(*SphericalPlot3D[1,{\[Theta],0,\[Pi]},{\[Phi],0,2 \[Pi]},*)
+(*ColorFunction->(gradients[t (.5+f[SphericalHarmonicY[l,m,#4,#5]])]&),Mesh->{17},MeshStyle->Opacity[0.2],Boxed->False,Axes->False,ColorFunctionScaling->False,PlotPoints->200,SphericalRegion->True,ViewAngle->.3,ImageSize->100]*)
+(*SphericalPlot3D[1,{\[Theta],0,\[Pi]},{\[Phi],0,2 \[Pi]},*)
+(*ColorFunction->(gradients[t (.5+f[Sqrt[(2l+1)/(2Pi) (l-m)!/(l+m)!]*(LegendreP[l,m,Cos[#4 ]]+ ((2*I)/Pi)LegendreQ[l,m,Cos[#4] ])*Exp[I*m*#5]])]&),Mesh->{17},MeshStyle->Opacity[0.2],*)
+(*Boxed->False,Axes->False,ColorFunctionScaling->False,PlotPoints->200,SphericalRegion->True,ViewAngle->.3,ImageSize->100]*)
+(*SphericalPlot3D[1,{\[Theta],0,\[Pi]},{\[Phi],0,2 \[Pi]},*)
+(*ColorFunction->(gradients[t (.5+f[Sqrt[(2l+1)/(2Pi) (l-m)!/(l+m)!]*(LegendreP[l,m,Cos[#4 ]]- ((2*I)/Pi)LegendreQ[l,m,Cos[#4] ])*Exp[I*m*#5]])]&),Mesh->{17},MeshStyle->Opacity[0.2],*)
+(*Boxed->False,Axes->False,ColorFunctionScaling->False,PlotPoints->200,SphericalRegion->True,ViewAngle->.3,ImageSize->100]*)
